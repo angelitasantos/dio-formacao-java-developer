e47fd1459cc8366e12de799e368e7392ea6dac9f
Angelita Santos <angelitasantos@gmail.com>
2022-07-11 21:39:02 -0300

    cria documentacao javadoc

---
+++ modulo01-fundamentos-java/atividade03-sintaxe/05doc/documentacao/src/Calculadora.java
@@ -0,0 +1,23 @@
+/**
+ * <h1>Calculadora
+ * <h1/>
+ * <b>Note:</b>
+ * Leia atentamente a documentação desta classe
+ * 
+ * @author Angelita Santos
+ * @version 1.0
+ * @since 11/07/2022
+ * 
+ */
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * 
+     * @param numeroUm   primeiro parametro do metodo
+     * @param numeroDois segundo parametro do metodo
+     * @return int o resultado deste método é a s oma dos oids numeros
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
