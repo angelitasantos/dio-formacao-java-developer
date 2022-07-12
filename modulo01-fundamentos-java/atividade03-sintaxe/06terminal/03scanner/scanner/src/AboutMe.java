c4d2ff1c803e707d1d220474270851597acbd860
Angelita Santos <angelitasantos@gmail.com>
2022-07-12 10:16:22 -0300

    utiliza terminal e argumentos

---
+++ modulo01-fundamentos-java/atividade03-sintaxe/06terminal/03scanner/scanner/src/AboutMe.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class AboutMe {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Informações Pessoais");
+
+        System.out.println("Digite seu nome ");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome ");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade ");
+        double idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura ");
+        double altura = scanner.nextDouble();
+
+        System.out.println("About Me");
+        System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos");
+        System.out.println("E minha altura é " + altura + " m");
+    }
+}
